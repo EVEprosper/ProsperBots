98c2b062e07a6141c1c70e455a3fc84b9420cf5b
lockefox <locke.renard@gmail.com>
2017-03-05 15:12:09 -0800

    adding tradier calendar coverage and tests

--- Tests/test_stocks.py
+++ Tests/test_stocks.py
@@ -3,13 +3,62 @@
 Pytest functions for exercising prosper.stocks packages
 
 """
-from os import path
+from os import path, remove, makedirs
+from datetime import datetime, timedelta
+import configparser
+
 import pytest
+from tinydb import TinyDB
 
 import prosper.stocks.remote as p_remote
+#import prosper.common.prosper_config as p_config
 
 HERE = path.abspath(path.dirname(__file__))
 ROOT = path.dirname(HERE)
+
+def get_config(config_path):
+    """parse config object for test information
+
+    Args:
+        config_path (str): path to config file (abspath > relpath)
+
+    Returns:
+        (:obj:`configparser.ConfigParser`) config object
+
+    """
+    #TODO: add stupid-parsing to common.prosper_config
+
+    local_config_path = config_path.replace('.cfg', '_local.cfg')
+    real_config_path = ''
+    if path.isfile(local_config_path):
+        real_config_path = local_config_path
+    else:
+        real_config_path = config_path
+
+    config = configparser.ConfigParser(
+        interpolation=configparser.ExtendedInterpolation(),
+        allow_no_value=True,
+        delimiters=('='),
+        inline_comment_prefixes=('#')
+    )
+
+    with open(real_config_path, 'r') as handle:
+        config.read_file(handle)
+
+    return config
+
+CONFIG = get_config(path.join(HERE, 'test_stocks.cfg'))
+CACHE_PATH = path.join(HERE, 'cache')
+makedirs(CACHE_PATH, exist_ok=True)
+def test_cleanup_testdir():
+    """make sure paths are cleaned up"""
+    for path_info in CONFIG.items('CACHE'):
+        try:
+            nuke_path = path.join(CACHE_PATH, path_info[1])
+            remove(nuke_path)
+        except Exception:
+            #don't fail, just try to clean up paths
+            pass
 
 def test_news_happypath():
     """test p_remote.get_news endpoint for happy-path"""
@@ -38,3 +87,248 @@ def test_news_emptyval():
     test_ticker = 'ABRN'
     with pytest.raises(p_remote.EmptyNewsEndpoint):
         data = p_remote.get_news(test_ticker)
+
+## TODO: add sequence test
+CALENDAR_CACHE = TinyDB(
+    path.join(HERE, CONFIG.get('CACHE', 'calendar_cache'))
+)
+HAS_AUTH = True
+def test_tradier_happypath():
+    """test regular path for tradier calendar utility"""
+    if not CONFIG.get('AUTH', 'tradier_api'):
+        global HAS_AUTH
+        HAS_AUTH = False
+        pytest.skip('No API key given')
+    status = p_remote.market_open(
+        CONFIG.get('AUTH', 'tradier_api'),
+        cache_buster=False,
+        calendar_cache=CALENDAR_CACHE
+    )
+
+def test_tradier_cachepath():
+    """make sure cache-only fetch works"""
+    if not HAS_AUTH:
+        pytest.skip('no way to validate without API')
+
+    status = p_remote.market_open(
+        CONFIG.get('AUTH', 'tradier_api'),
+        cache_buster=True,
+        calendar_cache=CALENDAR_CACHE
+    )
+
+def test_validate_market_open_status():
+    """make sure cache/remote agree
+
+    NOTE: relies on happypath success
+    """
+    if not HAS_AUTH:
+        pytest.skip('No way to validate status')
+
+    remote_status = p_remote.market_open(
+        CONFIG.get('AUTH', 'tradier_api'),
+        cache_buster=True
+    )
+
+    cache_value = p_remote.check_calendar_cache(
+        CALENDAR_CACHE
+    )
+
+    cache_status = None
+    if cache_value == p_remote.MarketStatus.OPEN:
+        cache_staus = True
+    else:
+        cache_status = False
+
+    assert cache_status == remote_status
+
+def test_fail_tradier_fetch():
+    """force fetch_tradier_calendar to fail"""
+    if not HAS_AUTH:
+        pytest.skip('No way to validate without API')
+
+    with pytest.raises(Exception):
+        p_remote.fetch_tradier_calendar(
+            CONFIG.get('AUTH', 'tradier_api'),
+            endpoint_url='http://api.eveprosper.com/noendpoint'
+        )
+
+def overwrite_calendar_status(
+        calendar_obj,
+        new_value
+):
+    """force 'status' to some other value"""
+    new_calendar = {}
+    new_calendar['calendar'] = {}
+    new_calendar['calendar']['days'] = {}
+    new_calendar['calendar']['days']['day'] = []
+    for date_row in calendar_obj['calendar']['days']['day']:
+        date_row['status'] = new_value
+        new_calendar['calendar']['days']['day'].append(date_row)
+
+    return new_calendar
+
+def test_fail_calendar_parse():
+    """try to validate a bad config"""
+    if not HAS_AUTH:
+        pytest.skip('No way to validate without API')
+
+    ## Find out-of-range day
+    today = datetime.today()
+    today_str = today.strftime('%Y-%m-%d')
+    next_month = today + timedelta(days=31)
+    next_month_str = next_month.strftime('%Y-%m-%d')
+
+    ## Make a calendar with bad values
+    good_calendar = p_remote.fetch_tradier_calendar(
+        CONFIG.get('AUTH', 'tradier_api')
+    )
+    bad_calendar = overwrite_calendar_status(
+        good_calendar,
+        'GARBAGE'
+    )
+
+    with pytest.raises(p_remote.UnexpectedMarketStatus):
+        status = p_remote.parse_tradier_calendar(
+            bad_calendar,
+            today_str
+        )
+        #assert status == p_remote.MarketStatus.UNKNOWN
+
+    with pytest.raises(p_remote.UnexpectedMarketStatus):
+        status = p_remote.parse_tradier_calendar(
+            good_calendar,
+            next_month_str
+        )
+        #assert status == p_remote.MarketStatus.UNKNOWN
+
+def test_force_open_calendar():
+    """make sure we test "OPEN" path"""
+    if not HAS_AUTH:
+        pytest.skip('No way to validate without API')
+
+    today = datetime.today()
+    today_str = today.strftime('%Y-%m-%d')
+    good_calendar = p_remote.fetch_tradier_calendar(
+        CONFIG.get('AUTH', 'tradier_api')
+    )
+    open_calendar = overwrite_calendar_status(
+        good_calendar,
+        'open'
+    )
+
+    status = p_remote.parse_tradier_calendar(
+        open_calendar,
+        today_str
+    )
+    assert status == p_remote.MarketStatus.OPEN
+
+def test_force_closed_calendar():
+    """make sure we test "CLOSED" path"""
+    if not HAS_AUTH:
+        pytest.skip('No way to validate without API')
+
+    today = datetime.today()
+    today_str = today.strftime('%Y-%m-%d')
+    good_calendar = p_remote.fetch_tradier_calendar(
+        CONFIG.get('AUTH', 'tradier_api')
+    )
+    closed_calendar = overwrite_calendar_status(
+        good_calendar,
+        'closed'
+    )
+
+    status = p_remote.parse_tradier_calendar(
+        closed_calendar,
+        today_str
+    )
+    assert status == p_remote.MarketStatus.CLOSED
+
+def close_and_reopen_cache(
+        tinydb_cache,
+        path_to_cache
+):
+    """force the tinydb cache closed and reopen it"""
+    tinydb_cache.close()
+    remove(path_to_cache)
+    new_db = TinyDB(path_to_cache)
+    return new_db
+
+def test_force_bad_cache():
+    """force bad cache into archive"""
+    if not HAS_AUTH:
+        pytest.skip('No way to validate without API')
+    global CALENDAR_CACHE
+    CALENDAR_CACHE = close_and_reopen_cache(
+        CALENDAR_CACHE,
+        path.join(HERE, CONFIG.get('CACHE', 'calendar_cache'))
+    )
+
+    good_calendar = p_remote.fetch_tradier_calendar(
+        CONFIG.get('AUTH', 'tradier_api')
+    )
+    bad_calendar = overwrite_calendar_status(
+        good_calendar,
+        'GARBAGE'
+    )
+
+    p_remote.update_calendar_cache(
+        CALENDAR_CACHE,
+        bad_calendar
+    )
+
+    with pytest.raises(p_remote.UnexpectedMarketStatus):
+        status = p_remote.check_calendar_cache(CALENDAR_CACHE)
+        print(status)
+def test_force_open_cache():
+    """force cache to "OPEN" status"""
+    if not HAS_AUTH:
+        pytest.skip('No way to validate without API')
+    global CALENDAR_CACHE
+    CALENDAR_CACHE = close_and_reopen_cache(
+        CALENDAR_CACHE,
+        path.join(HERE, CONFIG.get('CACHE', 'calendar_cache'))
+    )
+
+    good_calendar = p_remote.fetch_tradier_calendar(
+        CONFIG.get('AUTH', 'tradier_api')
+    )
+    open_calendar = overwrite_calendar_status(
+        good_calendar,
+        'open'
+    )
+
+    p_remote.update_calendar_cache(
+        CALENDAR_CACHE,
+        open_calendar
+    )
+
+    status = p_remote.check_calendar_cache(CALENDAR_CACHE)
+
+    assert status == p_remote.MarketStatus.OPEN
+
+def test_force_closed_cache():
+    """force cache to "CLOSED" status"""
+    if not HAS_AUTH:
+        pytest.skip('No way to validate without API')
+    global CALENDAR_CACHE
+    CALENDAR_CACHE = close_and_reopen_cache(
+        CALENDAR_CACHE,
+        path.join(HERE, CONFIG.get('CACHE', 'calendar_cache'))
+    )
+
+    good_calendar = p_remote.fetch_tradier_calendar(
+        CONFIG.get('AUTH', 'tradier_api')
+    )
+    closed_calendar = overwrite_calendar_status(
+        good_calendar,
+        'closed'
+    )
+
+    p_remote.update_calendar_cache(
+        CALENDAR_CACHE,
+        closed_calendar
+    )
+
+    status = p_remote.check_calendar_cache(CALENDAR_CACHE)
+
+    assert status == p_remote.MarketStatus.CLOSED
--- prosper/stocks/remote.py
+++ prosper/stocks/remote.py
@@ -2,10 +2,12 @@
 
 from os import path
 from datetime import datetime, timedelta
+from enum import Enum
 
 import ujson as json
 import demjson
 import requests
+from tinydb import Query
 
 from nltk import download
 import nltk.sentiment as sentiment
@@ -13,7 +15,7 @@ import pandas_datareader.data as web
 
 import prosper.common.prosper_logging as p_logging
 
-LOGGER = p_logging.DEFAULT_LOGGER
+DEFAULT_LOGGER = p_logging.DEFAULT_LOGGER
 
 HERE = path.abspath(path.dirname(__file__))
 
@@ -83,6 +85,222 @@ def get_news(
 
     return demjson.decode(req.text)
 
+class MarketStatus(Enum):
+    """enum for tracking market status"""
+    OPEN = 1
+    CLOSED = 2
+    UNKNOWN = 3
+
+def market_open(
+        auth_key,
+        cache_buster=False,
+        calendar_cache=None,
+        endpoint_url='https://api.tradier.com/v1/markets/calendar',
+        logger=DEFAULT_LOGGER
+):
+    """check to see if the market is open today
+
+    Note:
+        uses https://developer.tradier.com/documentation/markets/get-calendar
+
+    Args:
+        auth_key (str): Tradier API key
+        cache_buster (bool, optional): ignore cache, DEFAULT: False
+        calendar_cache (:obj:`TinyDB`, optional): cached version of market calendar
+        endpoint_url (str, optional): tradier endpoint (exposed for testing)
+        logger (:obj:`logging.logger`): logger for tracking progress
+    Returns:
+        (bool): is market open?
+
+    """
+    logger.info('testing if market is open today')
+    today = datetime.today().strftime('%Y-%m-%d')
+    status = MarketStatus.UNKNOWN
+
+    ## Check to see if we have the schedule already ##
+    if (not cache_buster) and (calendar_cache is not None):
+        status = check_calendar_cache(
+            calendar_cache,
+            logger
+        )
+
+    ## Get calendar from API ##
+    calendar = {}
+    if cache_buster or status == MarketStatus.UNKNOWN:
+        calendar = fetch_tradier_calendar(
+            auth_key,
+            endpoint_url,
+            logger
+        )
+        status = parse_tradier_calendar(
+            calendar,
+            today,
+            logger
+        )
+
+    ## Write update to cache ##
+    if (calendar_cache is not None) and calendar:
+        update_calendar_cache(
+            calendar_cache,
+            calendar,
+            logger
+        )
+
+    #TODO: test coverage?
+    if status == MarketStatus.OPEN:
+        return True
+    else:
+        return False
+
+def parse_tradier_calendar(
+        calendar_obj,
+        today,
+        logger=DEFAULT_LOGGER
+):
+    """parse calendar to find out if market is open today
+
+    Args:
+        calendar_obj (:obj:`dict`): raw output from tradier v1/markets/calendar
+        today (str): datetime string %Y-%m-%d for searching tradier output
+        logger (:obj:`logging.logger`, optional): logger for tracking progress
+
+    Returns:
+        (:enum:`MarketStatus`): current market status
+
+    """
+    status = MarketStatus.UNKNOWN
+    date_info = {}
+    for day in calendar_obj['calendar']['days']['day']:
+        if day['date'] == today:
+            date_info = day
+            break   #stop searching when found
+        else:
+            continue
+
+    if date_info:
+        if date_info['status'] == 'open':
+            status = MarketStatus.OPEN
+        elif date_info['status'] == 'closed':
+            status = MarketStatus.CLOSED
+        else:
+            logger.warning(
+                'Unable to classify market status' +
+                '\n\tstatus={0}'.format(date_info['status']) +
+                '\n\tdate_info={0}'.format(date_info)
+            )
+            raise UnexpectedMarketStatus(
+                '{0} is unhandled market status value'.format(date_info['status'])
+            )
+    else:
+        logger.error(
+            'EXCEPTION: unable to find date  info at Tradier' +
+            '\n\tcalendar={0}'.format(calendar_obj) +
+            '\n\ttoday={0}'.format(today)
+        )
+        raise UnexpectedMarketStatus('No information found for today')
+
+    return status
+
+def fetch_tradier_calendar(
+        auth_key,
+        endpoint_url='https://api.tradier.com/v1/markets/calendar',
+        logger=DEFAULT_LOGGER
+):
+    """Fetch latest calendar from Tradier
+
+    Args:
+        auth_key (str): Tradier API key
+        endpoint_url (str, optional): api URL path
+        logger (:obj:`logging.logger`, optional): logger for tracking progress
+
+    Returns:
+        (:obj:`dict`) Tradier API response
+
+    """
+    logger.info('--checking internet for calendar')
+
+    headers = {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + auth_key
+    }
+    try:
+        req = requests.get(
+            endpoint_url,
+            headers=headers
+        )
+        req.raise_for_status()
+        calendar = req.json()
+    except Exception as err_msg:
+        logger.error(
+            'EXCEPTION: unable to fetch calendar' +
+            '\n\turl={0}'.format(endpoint_url),
+            exc_info=True
+        )
+        raise err_msg #TODO: no calendar behavior?
+
+    return calendar
+
+def check_calendar_cache(
+        calendar_cache,
+        logger=DEFAULT_LOGGER
+):
+    """look for current day inside existing calendar tinyDB
+
+    Args:
+        calendar_cache (:obj:`tinydb.tinyDB`): collection of tradier market calendar info
+        logger (:obj:`logging.logger`): logging handle for debug stuff
+
+    Returns:
+        (:enum:MarketStatus) status of market today
+
+    """
+    logger.info('--checking cache')
+    status = MarketStatus.UNKNOWN
+    today = datetime.today().strftime('%Y-%m-%d')
+    day_query = Query()
+
+    cache_value = calendar_cache.search(day_query.date == today)
+    if cache_value:
+        logger.info('--found info in cache')
+        if cache_value[0]['status'] == 'closed':
+            logger.debug('----markets closed today')
+            status = MarketStatus.CLOSED
+        elif cache_value[0]['status'] == 'open':
+            logger.debug('----markets open today')
+            status = MarketStatus.OPEN
+        else:
+            #TODO: how to test?
+            logger.error(
+                'EXCEPTION: unexpected market status' +
+                '\n\tcache_value={0}'.format(cache_value)
+            )
+            raise UnexpectedMarketStatus(
+                'Could not resolve status ' +
+                '\n\tstatus={0}'.format(cache_value[0]['status'])
+            )
+
+    return status
+
+def update_calendar_cache(
+        calendar_cache,
+        update_payload,
+        logger=DEFAULT_LOGGER
+):
+    """function for pushing updated information into cache
+
+    Args:
+        calendar_cache (:obj:`tinydb.TinyDB`): db with cached calendar info
+        update_payload (:obj:`dict`): raw tradier output from v1/markets/calendar
+        logger (:obj:`logging.logger`, optional): logger for tracking progress
+
+    Returns:
+        void
+
+    """
+    logger.info('--updating cache')
+    calendar_update = update_payload['calendar']['days']['day']
+    calendar_cache.insert_multiple(calendar_update)
+
 class RemoteException(Exception):
     """base exception class for remote modules"""
     pass
@@ -91,4 +309,7 @@ class EmptyNewsEndpoint(RemoteException):
     pass
 class EndpointDown(RemoteException):
     """wrapper for request errors, alert to stop parsing"""
+    pass
+class UnexpectedMarketStatus(RemoteException):
+    """exception for alerting when market calendar reaches unknown state"""
     pass
