ebe4aea4da16bcebc1d296caee8147187867892a
John Purcell <john.purcell@crowdstrike.com>
2017-08-28 19:19:34 -0700

    updating project to latest python framework template

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
-"""wheel setup for Prosper bots"""
-
+"""wheel setup for Prosper Bot framework"""
+from codecs import open
+import importlib
 from os import path, listdir
+
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 HERE = path.abspath(path.dirname(__file__))
-__version__ = '0.0.1'
-#__project_name__ = 'bots'
+__package_name__ = 'ProsperBots'
+__library_name__ = 'bots'
+
+def get_version(package_name):
+    """find __version__ for making package
+
+    Args:
+        package_path (str): path to _version.py folder (abspath > relpath)
+
+    Returns:
+        (str) __version__ value
+
+    """
+    module = package_name + '._version'
+    package = importlib.import_module(module)
+
+    version = package.__version__
+
+    return version
 
 def hack_find_packages(include_str):
     """patches setuptools.find_packages issue
@@ -56,9 +75,11 @@ class PyTest(TestCommand):
     def initialize_options(self):
         TestCommand.initialize_options(self)
         self.pytest_args = [
-            'Tests',
-            '--cov=prosper/',
-            '--cov-report=term-missing'
+            'tests',
+            '-rx',
+            '--cov=' + __library_name__,
+            '--cov-report=term-missing',
+            '--cov-config=.coveragerc'
         ]    #load defaults here
 
     def run_tests(self):
@@ -74,18 +95,18 @@ class PyTest(TestCommand):
         exit(errno)
 
 setup(
-    name='ProsperBots',
+    name=__package_name__,
     author='John Purcell',
     author_email='prospermarketshow@gmail.com',
-    url='https://github.com/EVEprosper/ProsperBots',
-    download_url='https://github.com/EVEprosper/ProsperBots/tarball/v' + __version__,
-    version=__version__,
+    url='https://github.com/EVEprosper/' + __package_name__,
+    download_url='https://github.com/EVEprosper/' + __package_name__ + '/tarball/v' + get_version(__library_name__),
+    version=get_version(__library_name__),
     license='MIT',
     classifiers=[
         'Programming Language :: Python :: 3.5'
     ],
-    keywords='prosper eveonline api CREST',
-    packages=hack_find_packages('prosper'),
+    keywords='prosper nltk discord chat bot slack stocks',
+    packages=find_packages(__library_name__),
     include_package_data=True,
     data_files=[
         ('Tests', include_all_subfiles('Tests')),
@@ -93,27 +114,30 @@ setup(
         ('Scripts', include_all_subfiles('Scripts'))
     ],
     package_data={
-        'prosper':[
-            'DiscordBot/discord_config.cfg',
-            'SlackBot/slackbot_config.cfg'
-        ]
+
     },
     install_requires=[
-        'requests~=2.13.0',
-        'ProsperCommon~=0.4.0',
-        'ujson~=1.35',
-        'tinydb~=3.2.1',
-        'demjson~=2.2.4',
-        'discord.py~=0.13.0',
-        'nltk~=3.2.1',
-        'pandas-datareader~=0.2.1',
-        'slackbot~=0.4.1',
-        'six~=1.10.0'
+        'ProsperCommon~=1.1.0',
+        'ProsperDatareader[nltk]~=1.0.1',
+        'tinydb~=3.4.1',
+        'tinymongo~=0.1.9',
+        'discord.py~=0.16.10',
+        'slackbot~=0.5.1',
+        'pandas~=0.20.3',
+        'plotnine~=0.2.1'
     ],
     tests_require=[
         'pytest~=3.0.0',
         'pytest_cov~=2.4.0',
     ],
+    extras_require={
+        'dev':[
+            'pandas-datareader',
+            'sphinx',
+            'sphinxcontrib-napoleon',
+            'semantic-version'
+        ]
+    },
     cmdclass={
         'test':PyTest
     }
