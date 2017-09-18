cb568f57be3ff0ef3b19fb8c9ce6997d43143fc7
Lockefox <locke.renard@gmail.com>
2017-09-18 13:52:38 -0700

    adding test for version_info()

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,5 +1,6 @@
 """test_commands.py: validate core responses for bots"""
 from os import path
+import platform
 
 from parse import *
 import pytest
@@ -7,10 +8,24 @@ import tinymongo
 
 import prosper_bots.commands as commands
 import prosper_bots.exceptions as exceptions
+import prosper_bots._version as version
 
 HERE = path.abspath(path.dirname(__file__))
 ROOT = path.abspath(path.join(path.dirname(HERE), 'prosper_bots'))
 CACHE_PATH = path.join(HERE, 'cache')
+
+
+class TestVersionInfo:
+    """validate version_info() behavior"""
+    app_name = 'TestmodeApp'
+    def test_version_info_happypath(self):
+        response = commands.version_info(self.app_name)
+
+        values = search('{app_name} -- {version} -- {platform:S}', response)
+
+        assert values['app_name'] == self.app_name
+        assert values['version'] == version.__version__
+        assert values['platform'] == platform.node()
 
 class TestGenericStockInfo:
     """validate generic_stock_info behavior"""
@@ -24,7 +39,6 @@ class TestGenericStockInfo:
             self.conn
         )
         values = search('{company_name} {price:f} {change_pct:%}', response)
-
 
         assert values['company_name'] == 'Micron Technology, Inc. - Common Stock'
         assert isinstance(values['price'], float)
