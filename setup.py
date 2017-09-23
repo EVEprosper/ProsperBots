6375fbd90146f559399adfb39e167f8d4d18fb0f
Lockefox <locke.renard@gmail.com>
2017-09-22 17:04:32 -0700

    touching up setup.py for doc build

--- setup.py
+++ setup.py
@@ -136,7 +136,7 @@ setup(
     },
     install_requires=[
         'ProsperCommon~=1.1.1',
-        'ProsperDatareader[nltk]~=1.2.0a0',
+        'ProsperDatareader[nltk]~=1.2.0a1',
         'tinydb~=3.4.1',
         'tinymongo~=0.1.9',
         'discord.py~=0.16.10',
