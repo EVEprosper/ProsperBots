3bb1a21d9cab54ae977887d1ece05d584977e6c3
lockefox <locke.renard@gmail.com>
2017-11-07 18:58:20 -0800

    pegging pandas version b/c of issue with 0.21 compile issues

--- setup.py
+++ setup.py
@@ -142,7 +142,7 @@ setup(
         'discord.py~=0.16.10',
         'slackbot~=0.5.1',
         'plumbum~=1.6.3',
-        'pandas~=0.20.3',
+        'pandas==0.20.3',
         'pandas-datareader~=0.5.0',
         'requests-cache~=0.4.13',
         'contexttimer~=0.3.3',
